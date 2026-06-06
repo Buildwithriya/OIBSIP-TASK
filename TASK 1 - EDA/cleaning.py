5748b85f73b333cb821c4ead2d88e917e4f86cd5
riya <riya26082006@gmail.com>
2026-06-06 23:38:53 +0530

    cleaning the file

--- TASK 1 - EDA/cleaning.py
+++ TASK 1 - EDA/cleaning.py
@@ -4,12 +4,33 @@ df=pd.read_csv(r"C:\Users\Riya\OneDrive\Documents\OIBSIP\TASK 1 - EDA\retail_sal
 
 # initial inspection 
 
+print(df.shape)
 df.info() # getting the info of the dataset
 
-print(df.isnull().sum())  # checking of null values 
 
 print(df.head())
 
 
-print(df.describe()) # statistical summary 
+# fix the datatype
+df['Date']= pd.to_datetime(df['Date'])
+print(df.dtypes)
 
+# checking for missing / duplicate value
+print(df.isnull().sum())  # checking of null values 
+print(df.duplicated().sum()) # duplicated values
+
+# creating new column for month and quarter
+df['Year']=df['Date'].dt.to_period('Y')
+
+df['Month']=df['Date'].dt.to_period('M')
+df['Quarter']= df['Date'].dt.to_period('Q')
+
+df['Age_Group']=pd.cut(
+                        df['Age'],
+                        bins=[17,25,35,45,55,65,75],
+                        labels=['18-24', '25-34', '35-44', '45-54', '55-64', '65-74'])
+print(df.head())
+
+
+# descriptive statistics
+print(df[['Age', 'Quantity', 'Price per Unit', 'Total Amount']].describe().round(2))
