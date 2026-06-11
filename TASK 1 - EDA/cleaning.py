cf695048bb88d2f9a08221ac2152f2115a4b06c4
riya <riya26082006@gmail.com>
2026-06-11 16:16:43 +0530

    visualization using matplotlib and seaborn

--- TASK 1 - EDA/cleaning.py
+++ TASK 1 - EDA/cleaning.py
@@ -30,8 +30,8 @@ df['Quarter']= df['Date'].dt.to_period('Q')
 
 df['Age_Group']=pd.cut(
                         df['Age'],
-                        bins=[17,25,35,45,55,65,75],
-                        labels=['18-24', '25-34', '35-44', '45-54', '55-64', '65-74'])
+                        bins=[17,25,35,45,55,65],
+                        labels=['18-24', '25-34', '35-44', '45-54', '55-64'])
 print(df.head())
 
 
@@ -48,7 +48,7 @@ print(df.groupby('Product Category')['Total Amount'].sum())
 monthly = df.groupby('Month')['Total Amount'].sum().reset_index()
 
 monthly['Month_str'] = monthly['Month'].astype(str)
-monthly = monthly[monthly['Month_str'] < '2024-01']
+# monthly = monthly[monthly['Month_str'] < '2024-01']
 print(monthly)
 
 
@@ -84,3 +84,98 @@ top_customers=df.groupby('Customer ID')['Total Amount'].sum().nlargest(10)
 print(top_customers)
 
 # visualization
+# chart1 : monthly revenue 
+plt.figure(figsize=(12,6))
+sns.lineplot(data=monthly, x='Month_str', y='Total Amount', marker='o',color='blue',linewidth=2.5)
+plt.title('Monthly Revenue Trend',fontsize=18,fontweight='bold',color='darkblue',pad=20)
+plt.xlabel('Month', fontsize=14,fontweight='bold',color='darkblue')
+plt.ylabel('Total Revenue', fontsize=14,fontweight='bold',color='darkblue')
+
+for i, row in monthly.iterrows():
+    plt.text(row['Month_str'], row['Total Amount'] + 1000, f"₨ {int(row['Total Amount']):,}", 
+             ha='center', va='bottom', fontsize=9, fontweight='bold')
+
+plt.ylim(0, monthly['Total Amount'].max() * 1.2)
+plt.xticks(rotation=45)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+
+# chart 2: month over month change
+plt.figure(figsize=(12,6))
+sns.barplot(data=monthly, x='Month_str', y='MOM_Change', color='orange', edgecolor='black')
+plt.title('Month-over-Month Revenue Change (%)', fontsize=18, fontweight='bold', color='darkblue', pad=20)
+plt.xlabel('Month', fontsize=14, fontweight='bold', color='darkblue')
+plt.ylabel('MoM Change (%)', fontsize=14, fontweight='bold', color='darkblue')
+for i, row in monthly.iterrows():
+    plt.text(row['Month_str'], row['MOM_Change'] + 0.5, f"{row['MOM_Change']:.1f}%", 
+             ha='center', va='bottom', fontsize=9, fontweight='bold')
+plt.xticks(rotation=45)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+
+# chart 3 : revenue by category
+plt.figure(figsize=(12,6))
+sns.barplot(data=cat_analysis , x='Product Category', y='total_revenue',linewidth=2, edgecolor='black',palette='viridis', 
+    hue='Product Category',  legend=False)
+plt.title('Revenue by Product Category', fontsize=18, fontweight='bold', color='darkblue', pad=20)
+plt.xlabel('Product Category', fontsize=14, fontweight='bold', color='darkblue')
+plt.ylabel('Total Revenue', fontsize=14, fontweight='bold', color='darkblue')
+
+for i, row in cat_analysis.iterrows():
+    plt.text(i, row['total_revenue'] + 1000, f"₨ {int(row['total_revenue']):,}", 
+             ha='center', va='bottom', fontsize=9, fontweight='bold')
+plt.ylim(0, cat_analysis['total_revenue'].max() * 1.2)
+
+plt.xticks(rotation=45)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+# chart 4: gender breakdown
+plt.figure(figsize=(8,6))
+sns.barplot(data=gender_cat, x=gender_cat.index, y='Total Amount', color='lightcoral', edgecolor='black')
+plt.title('Revenue by Gender', fontsize=18, fontweight='bold', color='darkblue', pad=20)
+for i , row in gender_cat.iterrows():
+    plt.text(i, row['Total Amount'] + 1000, f"{int(row['Total Amount']):,}", 
+             ha='center', va='bottom', fontsize=9, fontweight='bold')
+plt.xlabel('Gender', fontsize=14, fontweight='bold', color='darkblue')
+plt.ylabel('Total Revenue', fontsize=14, fontweight='bold', color='darkblue')
+plt.xticks(rotation=45)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+
+# chart 5: age group analysis
+plt.figure(figsize=(12,6))
+sns.barplot(data=age_group_analysis.reset_index(), x='Age_Group', y='Total', color='lightgreen', edgecolor='black')
+plt.title('Revenue by Age Group', fontsize=18, fontweight='bold', color='darkblue', pad=20)
+for i, row in age_group_analysis.reset_index().iterrows():
+    plt.text(i, row['Total'] + 1000, f"₨ {int(row['Total']):,}", 
+             ha='center', va='bottom', fontsize=9, fontweight='bold')
+plt.xlabel('Age Group', fontsize=14, fontweight='bold', color='darkblue')
+plt.ylabel('Total Revenue', fontsize=14, fontweight='bold', color='darkblue')
+plt.xticks(rotation=45)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.show()
+
+
+# chart 6: top 10 customers
+
+plt.figure(figsize=(12,6))
+sns.barplot(x=top_customers.index, y=top_customers.values, color='skyblue', edgecolor='black')
+plt.title('Top 10 Customers by Total Revenue', fontsize=18, fontweight='bold', color='darkblue', pad=20)
+for i, value in enumerate(top_customers.values):
+    plt.text(i, value , f"₨ {int(value):,}", 
+             ha='center', va='bottom', fontsize=9, fontweight='bold')
+plt.xlabel('Customer ID', fontsize=14, fontweight='bold', color='darkblue')
+plt.ylabel('Total Revenue', fontsize=14, fontweight='bold', color='darkblue')
+plt.xticks(rotation=45)
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.tight_layout()
+plt.show()
